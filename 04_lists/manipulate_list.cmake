b75acfa49ff624bb6502571087b54f5aac805df6
Alex Kushnir <kushnir.alexander@gmail.com>
2025-04-30 15:23:59 +0300

    Add examples for scope and lists

---
+++ 04_lists/manipulate_list.cmake
@@ -0,0 +1,10 @@
+set(list1 A List With Several Elements)
+
+list(LENGTH list1 len)
+message("List1 length is ${len}")
+
+list(FIND list1 With found)
+message("The word With found at index ${found}")
+
+list(REMOVE_ITEM list1 Several)
+message("List1 after removal is ${list1}")
