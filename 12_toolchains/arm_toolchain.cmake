460660993e75d59003dc83363eee3a3c9b95ae4e
Alex Kushnir <kushnir.alexander@gmail.com>
2025-05-10 21:51:03 +0300

    Added toolchains example

---
+++ 12_toolchains/arm_toolchain.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++)
+set(CMAKE_AR arm-linux-gnueabi-ar)
+set(CMAKE_RANLIB arm-linux-gnueabi-ranlib)
+
