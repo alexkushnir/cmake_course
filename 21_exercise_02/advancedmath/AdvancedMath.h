28fd1c80bceac0e95aed3388cc78102244f446db
Alex Kushnir <kushnir.alexander@gmail.com>
2025-05-29 22:04:34 +0300

    Second exercise

---
+++ 21_exercise_02/advancedmath/AdvancedMath.h
@@ -0,0 +1,10 @@
+#pragma once 
+
+#include "BasicMath.h"
+
+namespace advancedmath
+{
+    double Power(double base, int exponent);
+    double Average(double value1, double value2);
+    int Factorial(int n);
+}
