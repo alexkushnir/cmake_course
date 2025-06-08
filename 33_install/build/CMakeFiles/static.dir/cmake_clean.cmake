1c5056672c6a253a474b11aee4a51459fc0ddbfc
Alex Kushnir <kushnir.alexander@gmail.com>
2025-06-08 20:25:48 +0300

    Added install example

---
+++ 33_install/build/CMakeFiles/static.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/static.dir/static_library.cpp.o"
+  "CMakeFiles/static.dir/static_library.cpp.o.d"
+  "libstatic.a"
+  "libstatic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
