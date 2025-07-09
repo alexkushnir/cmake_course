5565710f661bbca5f255007dd3494083ae6b6880
Alex Kushnir <kushnir.alexander@gmail.com>
2025-07-09 19:38:55 +0300

    Removed unneeded comment

--- 23_writing_find_module/Main.cpp
+++ 23_writing_find_module/Main.cpp
@@ -1,6 +1,6 @@
 #include <pqxx/pqxx>
 #include <iostream>
-// #include <source_location>
+
 int main()
 {
     std::cout << "libpqxx version: " << PQXX_VERSION_MAJOR << "." << PQXX_VERSION_MINOR << std::endl;
