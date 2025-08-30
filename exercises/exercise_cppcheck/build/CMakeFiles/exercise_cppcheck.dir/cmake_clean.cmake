0b1b0470a39cf03de8f45a8d800f5851b59088d6
Alex Kushnir <kushnir.alexander@gmail.com>
2025-08-30 22:21:38 +0300

    Added an exercise for cppcheck

---
+++ exercises/exercise_cppcheck/build/CMakeFiles/exercise_cppcheck.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exercise_cppcheck.dir/Main.cpp.o"
+  "CMakeFiles/exercise_cppcheck.dir/Main.cpp.o.d"
+  "exercise_cppcheck"
+  "exercise_cppcheck.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exercise_cppcheck.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
